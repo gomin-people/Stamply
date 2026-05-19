ff0132886065be6e7e9eef025d5b5011a2492b11
DevONew <kyl2778@naver.com>
2026-05-19 12:55:06 +0900

    feat: Modal 컴포넌트 기본 구조 추가

---
+++ components/user/Modal.tsx
@@ -0,0 +1,14 @@
+// 'use client'
+// import { ReactNode } from "react"
+
+// type ModalProps = {
+//     isOpen: boolean
+//     onClose: () => void
+//     children: ReactNode
+// }
+
+// const Modal = ({ isOpen, onClose, children}: ModalProps) => {
+//     return(
+        
+//     )
+// }
