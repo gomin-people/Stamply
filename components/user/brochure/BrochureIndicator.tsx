1d9238cd47ff53477bf6f0842a9acf472479dd2e
DevONew <kyl2778@naver.com>
2026-05-26 17:47:09 +0900

    fix: 인디케이터 색상 변경 & 한개일때 표시

--- components/user/brochure/BrochureIndicator.tsx
+++ components/user/brochure/BrochureIndicator.tsx
@@ -6,13 +6,19 @@ type BrochureIndicatorProps = {
 const BrochureIndicator = ({ total, currentIndex }: BrochureIndicatorProps) => {
   const count = Math.min(total, 10)
 
-  return total > 1 && (
+  if (total === 1) {
+    return (
+      <div className="w-75 h-1.5 rounded-[10px] bg-(--primary-color)" />
+    )
+  }
+
+  return (
     <div className="flex gap-3 w-75">
       {Array.from({ length: count }).map((_, index) => (
         <div
           key={index}
           className={`flex-1 h-1.5 rounded-[10px] transition-colors duration-300 ${
-            index === currentIndex ? 'bg-gomin-primary-700' : 'bg-gomin-neutral-300'
+            index === currentIndex ? 'bg-(--primary-color)' : 'bg-gomin-neutral-300'
           }`}
         />
       ))}
