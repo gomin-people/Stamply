d412bc4a9e0100ed03244b0238b22fead3172d6f
hyeonmi <kim.hyeonmi.0711@gmail.com>
2026-05-26 09:58:48 +0900

    fix: 컴포넌트 파일 이름
    - logo -> Logo 변경시 git에서 대소문자 구분 못하는 버그가 있어서 StamplyLogo로 변경

---
+++ components/admin/StamplyLogo.tsx
@@ -0,0 +1,7 @@
+export default function StamplyLogo() {
+  return (
+    <h1 className="text-4xl text-gomin-primary-700 tracking-tight [font-family:var(--font-monomaniac-one)]">
+      Stamply
+    </h1>
+  );
+}
