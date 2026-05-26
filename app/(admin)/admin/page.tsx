7f4392efa8316a83e7cbbea247cb5d69cec925ef
skylarkim22 <kim.hyeonmi.0711@gmail.com>
2026-05-26 10:18:20 +0900

    fix: 컴포넌트 파일 이름 (#53)
    - logo -> Logo 변경시 git에서 대소문자 구분 못하는 버그가 있어서 StamplyLogo로 변경

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -1,5 +1,5 @@
-import KakaoLogin from "@/components/admin/kakao-login";
-import Logo from "@/components/admin/logo";
+import KakaoLogin from '@/components/admin/KakaoLogin';
+import StamplyLogo from '@/components/admin/StamplyLogo';
 
 export default function AdminHomePage() {
   return (
@@ -7,13 +7,12 @@ export default function AdminHomePage() {
       <div className="w-full max-w-125 bg-white rounded-2xl shadow-sm px-12 py-12 flex flex-col items-center gap-8">
         {/* Header */}
         <div className="flex flex-col items-center gap-2">
-          <Logo />
+          <StamplyLogo />
           <p className="text-base text-[#6D6D6D]">행사 운영 관리자 로그인</p>
         </div>
 
         <KakaoLogin />
       </div>
-
     </main>
   );
 }
--- components/admin/logo.tsx
+++
@@ -1,5 +0,0 @@
-export default function Logo() {
-    return <h1 className="text-4xl text-gomin-primary-700 tracking-tight [font-family:var(--font-monomaniac-one)]">
-        Stamply
-    </h1>
-}
