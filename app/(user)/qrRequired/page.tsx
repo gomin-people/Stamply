2cf33245ff24857ff69cc4c79fbab38f9a000a5e
DevONew <kyl2778@naver.com>
2026-05-26 14:12:32 +0900

    fix: qr 접근 페이지 수정

--- app/(user)/qrRequired/page.tsx
+++ app/(user)/qrRequired/page.tsx
@@ -1,7 +1,7 @@
 const QrRequiredPage = () => {
     return (
         <main className="min-h-screen bg-white flex justify-center">
-            <div className="w-full max-w-100.5 flex flex-col items-center justify-center px-12">
+            <div className="w-full max-w-100.5 flex flex-col items-center justify-center px-12 -translate-y-25">
                 <p className="font-sans text-base font-bold leading-normal text-center">
                     QR을 찍고 입장해주세요.
                 </p>
