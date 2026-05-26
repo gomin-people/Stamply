f72df2fd48803fe71fe4618ecb29356a972f3ffc
DevONew <kyl2778@naver.com>
2026-05-26 18:14:45 +0900

    feat: proxy.ts 사용하여 쿠키 검증 추가

--- app/(user)/qr-required/page.tsx
+++ app/(user)/qr-required/page.tsx
@@ -3,7 +3,7 @@ const QrRequiredPage = () => {
         <main className="min-h-screen bg-white flex justify-center">
             <div className="w-full max-w-100.5 flex flex-col items-center justify-center px-12 -translate-y-25">
                 <p className="font-sans text-base font-bold leading-normal text-center">
-                    QR을 찍고 입장해주세요.
+                    입장 QR을 찍고 접속해주세요.
                 </p>
             </div>
         </main>
--- proxy.ts
+++ proxy.ts
@@ -1,10 +1,21 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+import { PARTICIPANT_COOKIE_NAME } from '@/utils/api';
 
-export function proxy() {
-  // TODO: 어드민 세션/토큰 검증 로직 추가
+export function proxy(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  if (pathname.match(/^\/event\/[^/]+\/brochure/)) {
+    const participantCookie = request.cookies.get(PARTICIPANT_COOKIE_NAME);
+    if (!participantCookie) {
+      return NextResponse.redirect(new URL('/qr-required', request.url));
+    }
+  }
+
   return NextResponse.next();
 }
 
+// 우선 브로셔 페이지만 쿠키 없을때 미들웨어 처리를 했습니다. 엔트리랑 다른 페이지들은 2차 개발로 미루겠습니다.
+// 아니면 우진님 미션 페이지 때 사용하셔도 됩니다. 
 export const config = {
-  matcher: ['/admin/:path*'],
+  matcher: ['/event/:eventId/brochure'],
 };
