3016778992c680807e0b0cf71476b059f71544c7
DevONew <kyl2778@naver.com>
2026-05-27 14:50:04 +0900

    fix: 리베이스 후 1번째 단계 추가

--- components/admin/event/EventStep1Form.tsx
+++
@@ -1,7 +0,0 @@
-export default function EventStep1Form() {
-  return (
-    <div className="flex min-h-[400px] items-center justify-center text-sm text-gomin-neutral-400">
-      행사정보 · 포스터
-    </div>
-  );
-}
