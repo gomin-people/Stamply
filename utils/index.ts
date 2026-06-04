111f73dd765d035aa9688c258d6b2ec28103ddb3
hyeonmi <kim.hyeonmi.0711@gmail.com>
2026-06-04 17:19:45 +0900

    fix: 주소 입력폼에 XSS 공격 코드 넣지 못하도록 변경

--- utils/index.ts
+++ utils/index.ts
@@ -37,8 +37,8 @@ export const isValidEmail = (value: string): boolean =>
 
 export const isValidUrl = (value: string): boolean => {
   try {
-    new URL(value);
-    return true;
+    const url = new URL(value);
+    return url.protocol === "http:" || url.protocol === "https:";
   } catch {
     return false;
   }
