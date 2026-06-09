2ee1ad88ba71e728bdefa2282640c1dbb2bdd299
hyeonmi <kim.hyeonmi.0711@gmail.com>
2026-06-09 20:12:38 +0900

    refact: 이미지 업로드 hook 추가 및 포스터 업로드 적용

--- components/admin/event/EventThemeStampForm.tsx
+++ components/admin/event/EventThemeStampForm.tsx
@@ -6,6 +6,7 @@ import { generatePalette, hslToHex, hexToHsl } from "@/utils";
 import StampUploadSection from "./themeStamp/StampUploadSection";
 import ThemeColorPicker from "./themeStamp/ThemeColorPicker";
 import ThemePreviewPanel from "./themeStamp/ThemePreviewPanel";
+import { toast } from "sonner";
 
 type InitialData = {
   stampImageUrl?: string | null;
@@ -19,11 +20,8 @@ type Props = {
 
 const EventThemeStampForm = forwardRef<StepFormHandle, Props>(
   function EventThemeStampForm({ initialData, disabled = false }, ref) {
-    const [stampPreviewUrl, setStampPreviewUrl] = useState<string | null>(
-      initialData?.stampImageUrl ?? null
-    );
-    const [stampFileUrl, setStampFileUrl] = useState<string | null>(
-      initialData?.stampImageUrl ?? null
+    const [stampFileUrl, setStampFileUrl] = useState<string>(
+      initialData?.stampImageUrl ?? ""
     );
     const [isUploading, setIsUploading] = useState(false);
 
@@ -48,12 +46,15 @@ const EventThemeStampForm = forwardRef<StepFormHandle, Props>(
       }
     }, [keyColor]);
 
+    const handeStampRemove = () => setStampFileUrl("");
+
     // 부모 컴포넌트에 넘길 validate 및 getData 정의
     useImperativeHandle(ref, () => ({
       validate: () => {
         if (isUploading) {
-          alert(
-            "스탬프 모양 이미지가 스토리지에 업로드 중입니다. 잠시만 기다려주세요."
+          toast.warning(
+            "스탬프 모양 이미지가 스토리지에 업로드 중입니다. 잠시만 기다려주세요.",
+            { id: "uploading" }
           );
           return false;
         }
@@ -70,10 +71,10 @@ const EventThemeStampForm = forwardRef<StepFormHandle, Props>(
         {/* 좌측 컨트롤 영역 */}
         <div className="flex-1 space-y-8">
           <StampUploadSection
-            stampPreviewUrl={stampPreviewUrl}
-            onPreviewChange={setStampPreviewUrl}
-            onFileUrlChange={setStampFileUrl}
+            value={stampFileUrl}
             onUploadingChange={setIsUploading}
+            onChange={setStampFileUrl}
+            onRemove={handeStampRemove}
             disabled={disabled}
           />
           <hr className="border-gomin-neutral-100" />
@@ -87,7 +88,7 @@ const EventThemeStampForm = forwardRef<StepFormHandle, Props>(
 
         {/* 우측 실시간 미리보기 영역 */}
         <ThemePreviewPanel
-          stampImage={stampPreviewUrl} // 실시간 모바일 미리보기에는 로컬 Object URL(지연 0초)을 즉시 렌더링
+          stampImage={stampFileUrl} // 실시간 모바일 미리보기에는 로컬 Object URL(지연 0초)을 즉시 렌더링
           palette={palette}
         />
       </div>
