504b56edba2b2f3a2fa9014373f0d1afc9930e3a
DevONew <kyl2778@naver.com>
2026-06-10 13:30:33 +0900

    fix: 브로셔 쪽 로딩 페이지 제거 및 미션 쪽 로딩 페이지 추가

--- app/(user)/event/[eventId]/mission/loading.tsx
+++ app/(user)/event/[eventId]/mission/loading.tsx
@@ -1,5 +1,5 @@
 import LoadingSpinner from "@/components/ui/loading-spinner";
 
-export default function BrochureLoading() {
+export default function MissionLoading() {
   return <LoadingSpinner />;
 }
